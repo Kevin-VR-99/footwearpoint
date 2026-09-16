3af5229426cc31b0ec2381a0e5c6596ea30c652a
PacoTh3Taco <aureliopaniagua6@gmail.com>
2026-09-15 20:46:42 -0600

    feat(backend): incluir pedido de revendedor en ciclo de compra vigente (E9-04)

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
-Route::get('/ping', function () {
+Route::post('/ping', function () {
     return response()->json(['status' => 'ok']);
 });
 
