9f3cf44e56457e4ac1f0d1fd99f3c237ac2a8599
Ailton Lievano Santiesteban <ailtonlievano@gmail.com>
2026-09-18 00:13:32 -0600

    Correccion E9-03: quitar import sin usar en la pantalla de pedido de revendedor TG-161

--- mobile/lib/screens/pedido_revendedor_screen.dart
+++ mobile/lib/screens/pedido_revendedor_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../models/producto_catalogo.dart';
 import '../models/item_pedido_revendedor.dart';
 import '../providers/auth_provider.dart';
 import '../services/api_service.dart';
